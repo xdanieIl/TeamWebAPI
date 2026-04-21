520b619d87ca1b95d9e1f7f10a9b12d7caaa0303
Daniel Moha <danimoha4132@outlook.com>
2026-04-20 23:38:48 -0400

    Add project files.

---
+++ TeamWebAPI/Controllers/TeamMembersController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TeamWebAPI.Data;
+using TeamWebAPI.Models;
+
+namespace TeamWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamMembersController : ControllerBase
+    {
+        private readonly AppDBContext _context;
+
+        public TeamMembersController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TeamMember>>> GetTeamMembers(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return await _context.TeamMembers.Take(5).ToListAsync();
+            }
+
+            var member = await _context.TeamMembers.FindAsync(id);
+
+            if (member == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(member);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TeamMember>> PostTeamMember(TeamMember member)
+        {
+            _context.TeamMembers.Add(member);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTeamMembers), new { id = member.Id }, member);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTeamMember(int id, TeamMember member)
+        {
+            if (id != member.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(member).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTeamMember(int id)
+        {
+            var member = await _context.TeamMembers.FindAsync(id);
+
+            if (member == null)
+            {
+                return NotFound();
+            }
+
+            _context.TeamMembers.Remove(member);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ TeamWebAPI/Data/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using TeamWebAPI.Models;
+
+namespace TeamWebAPI.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TeamMember> TeamMembers => Set<TeamMember>();
+        public DbSet<Hobby> Hobbies => Set<Hobby>();
+    }
+}
